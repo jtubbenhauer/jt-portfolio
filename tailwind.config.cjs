50b90a0e0e14f88e6d33d8cec53fc1210914da0e
Jack <jack@nimbusvapour.com.au>
2022-07-20 22:35:55 +1000

    Added navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    colors: {
+      'darkblue': '#20222c',
+      'pink': '#f73d93'
+    },
   },
   plugins: [],
 }
