7cfc59c799cbb61c5499153b9a453b67f1f615ca
Jack <jack@nimbusvapour.com.au>
2022-07-21 14:57:58 +1000

    Added about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,11 @@ function About() {
     <div className="flex flex-col items-center mt-4">
       <h2 className="text-3xl text-pink font-bold">About Me</h2>
       <p className="text-slate-300 p-4">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-        eaque eligendi est exercitationem modi nam optio quaerat ratione,
-        reiciendis sint!
+        I'm a web developer based in Sydney, Australia. I have a background in
+        building and operating successful ecommerce stores, and have developed a
+        passion for coding and web design along the way.
       </p>
+      <h3 className="text-2xl text-pink font-bold">Tech Stack</h3>
     </div>
   );
 }
