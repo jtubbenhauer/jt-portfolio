0425a03a0ab21ce39aad2ef31450998603fdddf2
Jack <jack@nimbusvapour.com.au>
2022-07-25 16:26:46 +1000

    Added basic contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,15 +3,37 @@ import { useState } from "react";
 
 function Contact() {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
+  };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+      <h2 className="text-3xl font-bold text-pink text-center">Get In Touch</h2>
+      <form className="flex flex-col items-center gap-2">
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <textarea
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={formSubmit}>Send</button>
+      </form>
     </div>
   );
 }
