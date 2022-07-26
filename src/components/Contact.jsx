4be2a3f7bd636a2bdefd63f8a8bfde822eb446e9
Jack <jack@nimbusvapour.com.au>
2022-07-26 19:15:33 +1000

    Styled contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,26 +13,36 @@ function Contact() {
 
   return (
     <div>
-      <h2 className="text-3xl font-bold text-pink text-center">Get In Touch</h2>
-      <form className="flex flex-col items-center gap-2">
+      <h2 className="text-3xl font-bold text-pink text-center mb-5">
+        Get In Touch
+      </h2>
+      <form className="flex flex-col items-center gap-3">
         <input
           type="text"
           placeholder="Name"
           value={name}
+          className="border-pink rounded p-2 bg-slate-700 text-slate-100 placeholder:text-slate-300"
           onChange={(e) => setName(e.target.value)}
         />
         <input
           type="text"
           placeholder="Email"
           value={email}
+          className="border-pink rounded p-2 bg-slate-700 text-slate-100 placeholder:text-slate-300"
           onChange={(e) => setEmail(e.target.value)}
         />
         <textarea
           placeholder="Message"
           value={message}
+          className="border-pink rounded p-2 bg-slate-700 text-slate-100 placeholder:text-slate-300"
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button onClick={formSubmit}>Send</button>
+        <button
+          onClick={formSubmit}
+          className="rounded-lg mt-2 py-2 px-4 font-bold text-lg border-2 border-solid border-pink text-slate-200"
+        >
+          Send
+        </button>
       </form>
     </div>
   );
