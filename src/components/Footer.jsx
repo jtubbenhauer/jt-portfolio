4336ae71992006e625ce7a50b85f0c18afc0fda8
Jack <jack@nimbusvapour.com.au>
2022-08-16 16:49:33 +1000

    Added footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="w-full bg-[#2b3545] text-slate-200 h-12 mt-14 flex items-center justify-center text-center">
+    <div className="w-full bg-[#2b3545] text-slate-200 h-12 mt-14 flex items-center justify-center text-center p-10 text-sm md:text-base">
       <p>
         Designed by Jack Tubbenhauer | Built with React & Tailwind |{" "}
-        <a href="#">GitHub Repo</a>
+        <a
+          href="https://github.com/jtubbenhauer/jt-portfolio"
+          className="text-pink"
+          target="_blank"
+        >
+          GitHub Repo
+        </a>
       </p>
     </div>
   );
